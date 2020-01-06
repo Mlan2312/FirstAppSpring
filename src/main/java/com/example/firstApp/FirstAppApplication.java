e90b27a7dc02c991369a6089e3289b44ce085911
Mlan2312 <59289870+Mlan2312@users.noreply.github.com>
2020-01-06 13:48:02 -0500

    Update FirstAppApplication.java

--- src/main/java/com/example/firstApp/FirstAppApplication.java
+++ src/main/java/com/example/firstApp/FirstAppApplication.java
@@ -8,7 +8,7 @@ public class FirstAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAppApplication.class, args);
-		System.out.println("Hello Spring user");
+		//System.out.println("Hello Spring user");
 	}
 
 }
