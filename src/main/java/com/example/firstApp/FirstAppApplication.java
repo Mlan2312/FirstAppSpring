cf5c8921b1dfd070a2d27e222fd121c12ccbe963
Milan <mpatel1mss@gmail.com>
2020-01-06 12:24:10 -0500

    First commit here

---
+++ src/main/java/com/example/firstApp/FirstAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+		System.out.println("Hello Spring user");
+	}
+
+}
